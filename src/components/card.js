e1a03db55df366cb1f36d1cf93beb33f1b5180c6
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-02-12 23:18:23 -0700

    Finished project

--- src/components/card.js
+++ src/components/card.js
@@ -57,103 +57,23 @@ import axios from 'axios';
   // However, the articles do not come organized in a single, neat array. Inspect the response closely!
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
-  
-
-  //I need to loop over each of the articles, then loop over each of the individual articles within.
 
   const cardAppender = (selector) => {
     axios.get(`http://localhost:5000/api/articles`)
     .then(resp => {
-      //console.log(resp);
-      // console.log(resp.data.articles[0])
       const entryPoint = document.querySelector(selector);
-      const data = resp.data.articles.javascript;
-
-      // resp.data.articles.forEach(article => {
-      // const dogCard = Card({ article });
-      // entryPoint.appendChild(dogCard);
-      // })
-
-
-    //   const breeds = Object.keys(resp.data.articles);
-    // for (let i = 0; i < breeds.length; i++) {
-    //   Card(breeds);
-    // }
-
-
-      // const keys = Object.keys(resp.data.articles);
-      // console.log(keys[0]);
-      // const values = Object.values(keys);
-      // console.log('the values:', values);
-
-      // values.forEach(obj => {
-      //   const elem = Card(obj);
-      //   entryPoint.appendChild(elem);
-      // })
-      
-
-      // breeds.forEach(obj => {
-      //   const elem = Card(obj);
-      //   entryPoint.appendChild(elem);
-      // })
-
-      // for (let i = 0; i < data.length; i++) {
-      //   const dataReturned = resp.data.articles[i];
-      //   console.log('data returned:', dataReturned);
-
-        // dataReturned.forEach(obj => {
-        //   const elem = Card(obj);
-        //   entryPoint.appendChild(elem);
-      // })
-    // }
-  })
-  .catch(err => {
-    console.log(err);
-  })
-
-}
+      const keys = Object.keys(resp.data.articles);
+      for (let i = 0; i < keys.length; i++) {
+        resp.data.articles[keys[i]].forEach(article => {
+          entryPoint.appendChild(Card(article));
+        })
+      }
+      })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+    
 
 
 export { Card, cardAppender }
-
-
-
-
-
-
-
-
-
-
-
-
-// const testData = 
-//   {"javascript":
-//     [
-//       {
-//       "id":"1e4d8724-5973-4b5b-84d9-a30a3c5adb70",
-//       "headline":"ES8: The Next Step in the Evolution of Javascript and What it Means For Your Projects",
-//       "authorPhoto":"https://tk-assets.lambdaschool.com/08d1372e-e393-47f1-ac44-fcb7d0baf0e2_sir.jpg",
-//       "authorName":"SIR RUFF'N'STUFF"
-//       },
-//       {
-//         "id":"553e2401-c95d-4029-89b8-fc6bdb60ae5e","headline":"Type Coercion: Why Does NaN !== NaN? and Other Strange Occurrences",
-//       "authorPhoto":"https://tk-assets.lambdaschool.com/a9471235-ed71-4b11-ae15-5a4fa1151d30_bones.jpg",
-//       "authorName":"BONES R. LIFE"
-//       },
-//       {
-//         "id":"9c2ed89e-7150-4cd6-a5c2-4a4f3d6932c8",
-//         "headline":"When to Rest, When to Spread: Why There Are Two Meanings Behind '...'",
-//         "authorPhoto":"https://tk-assets.lambdaschool.com/44260ce3-c8f0-4db8-bc1d-9877662fdf96_puppers.jpg",
-//         "authorName":"PUPPER S. DOGGO"
-//       },
-//       {
-//         "id":"b3af02ec-5733-4a14-8876-5db708d20051","headline":"Typescript: Ten Things you Should Know Before Building Your Next Angular Application",
-//         "authorPhoto":"https://tk-assets.lambdaschool.com/08d1372e-e393-47f1-ac44-fcb7d0baf0e2_sir.jpg",
-//         "authorName":"SIR RUFF'N'STUFF"
-//       }
-//     ],
-//   "bootstrap":
-//     [
-//       {
-//         "id":"5efa1378-c91f-49eb-9955-02e477c95920","headline":"Bootstrap 5: Get a Sneak Peak at all the New Features","authorPhoto":"https://tk-assets.lambdaschool.com/1891c758-b3f4-4ec7-9d88-590bf7c7ceb2_fido.jpg","authorName":"FIDO WALKSALOT"},{"id":"d03affc6-b06e-4b4a-9909-3981c7cb8389","headline":"UI Frameworks: A Comparison, Which Made Our List? (Hint: Bootstrap is on it)","authorPhoto":"https://tk-assets.lambdaschool.com/247dd20f-7e32-479b-a861-c3bbe85b5f0c_max.jpg","authorName":"MAX GOODBOYE"},{"id":"a85f88d3-0845-426d-9f02-930daa5c9b34","headline":"The Hottest New Bootstrap 4 Components Every Developer Needs to Know About","authorPhoto":"https://tk-assets.lambdaschool.com/247dd20f-7e32-479b-a861-c3bbe85b5f0c_max.jpg","authorName":"MAX GOODBOYE"}]
